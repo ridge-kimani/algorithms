3e177d7ba922ac4e09c9cecad5b9033943b24acf
Ridge Kimani <ridgekimani@gmail.com>
2023-11-01 21:19:28 +0300

    prettier fix

--- src/quantum/entanglement.ts
+++ src/quantum/entanglement.ts
@@ -1,30 +1,33 @@
-import * as qiskit from 'qiskit';
-import { Aer, QuantumCircuit, transpile } from 'qiskit';
-import { plot_histogram } from 'qiskit';
+import * as qiskit from 'qiskit'
+import { Aer, QuantumCircuit, transpile } from 'qiskit'
+import { plot_histogram } from 'qiskit'
 
-function createEntangledState(qubits: number = 2, shots: number = 1000): Record<string, number> {
+function createEntangledState(
+  qubits: number = 2,
+  shots: number = 1000
+): Record<string, number> {
   // Create a quantum circuit
-  const circuit = new QuantumCircuit(qubits, qubits);
+  const circuit = new QuantumCircuit(qubits, qubits)
 
   // Apply H gate to the first qubit
-  circuit.h(0);
+  circuit.h(0)
 
   // Apply a series of CNOT gates to create entanglement
   for (let i = 1; i < qubits; i++) {
-    circuit.cx(i - 1, i);
+    circuit.cx(i - 1, i)
   }
 
   // Measure all qubits
-  circuit.measureAll();
+  circuit.measureAll()
 
   // Simulate the circuit
-  const simulator = Aer.getBackend('aer_simulator');
-  const transpiledCircuit = transpile(circuit, simulator);
-  const job = simulator.run(transpiledCircuit, { shots: shots });
+  const simulator = Aer.getBackend('aer_simulator')
+  const transpiledCircuit = transpile(circuit, simulator)
+  const job = simulator.run(transpiledCircuit, { shots: shots })
 
   // Get the measurement counts
-  const result = job.result();
-  const counts = result.getCounts(circuit);
+  const result = job.result()
+  const counts = result.getCounts(circuit)
 
-  return counts;
+  return counts
 }
